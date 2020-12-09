82ea09d8e92d3bd2bbb5db9584362b386a86bec5
lukaslachowitz <73109755+lukaslachowitz@users.noreply.github.com>
2020-12-09 11:55:52 +0100

    Neu

--- Regex-Songetext/Program.cs
+++ Regex-Songetext/Program.cs
@@ -10,6 +10,56 @@ namespace Regex_Songetext
     {
         static void Main(string[] args)
         {
+            using (StreamReader reader = new StreamReader("TextFile1.txt"))
+            {
+                string zeile;
+                Regex rx = new Regex("ein");
+                while ((zeile = reader.ReadLine()) != null)
+                {
+                    MatchCollection matchCollection = rx.Matches(zeile);
+                    System.Diagnostics.Debug.WriteLine(matchCollection.Count);
+                    foreach (Match item in matchCollection)
+                    {
+
+
+                        // nimm das Code-snippet und mache eine Funktion daraus (am besten übergibst Du nur den String der Regular Expression) 
+                        // und dann:
+
+                        // Zähle folgende Strings in der Datei:
+                        // bar
+                        // Bar
+                        // bar oder Bar
+
+                        // alive
+                        // Alive
+                        // alive oder Alive
+
+                        // alle Zeilen die entweder
+                        // You got me rockin and a-rollin
+                        // oder 
+                        //Rockin and a - reelin
+
+                        // "brother" oder "mother" aber sonst nichts
+
+                        // evolution ohne ein "r" davor
+
+                        // "you" oder "you're" aber kein "your"  
+
+                        // know am Ende einer Zeile
+
+                        // wieviele Worte beginnen mit jedem buchstaben des Alphabets- daher Anzahl aller *Worte* die mit "a" beginnen (case-insensitive), b, c, d ...
+
+                        // komplizierter: gib die Zeilen aus die 
+                        // Bands und Liedertitle enthalten
+                        // nur die Bands
+                        // nur die Liedertitel
+                        // die Anzahl der Zeilen jedes Liedtitles
+                    }
+
+
+
+                }
+            }
         }
     }
 }
